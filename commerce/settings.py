36d50f050d0592c2ab673e5fddb54f7518f8059e
savyjeremiah <saviourjeremiah2024@gmail.com>
2025-01-02 12:08:36 -0800

    saviouri

--- commerce/settings.py
+++ commerce/settings.py
@@ -122,3 +122,16 @@ PAYPAL_RECEIVER_EMAIL = 'sb-dziio35804310@business.example.com'
 PAYPAL_TEST = True  
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+
+
+
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
+
+application = get_wsgi_application()
+
+
