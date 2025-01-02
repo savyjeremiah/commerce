02a5feb0842817aaa76247087f1caa2678d48f31
savyjeremiah <saviourjeremiah2024@gmail.com>
2025-01-02 05:06:49 -0800

    schooll

--- commerce/settings.py
+++ commerce/settings.py
@@ -1,5 +1,12 @@
 import os
 from pathlib import Path
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
+
+application = get_wsgi_application()
+
 
 # Define BASE_DIR
 BASE_DIR = Path(__file__).resolve().parent.parent
