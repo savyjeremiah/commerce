29c76ce4d707f4ded504b8ab6a46f0d4fd76190e
savyjeremiah <saviourjeremiah2024@gmail.com>
2025-01-02 13:15:16 -0800

    saviour

--- myapp/app.py
+++ myapp/app.py
@@ -1,4 +1,4 @@
-from app import app  # Assuming your Flask app instance is in app.py
+from commerce.app import app  # Assuming your Flask app instance is in app.py
 
 if __name__ == "__main__":
     app.run()
